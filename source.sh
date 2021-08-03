bd5b3924ad26dfaaad92585875ec7f5752da7624
FeverDreme <jeffreyhuang08@gmail.com>
2021-08-03 02:24:07 -0400

    added build from source script

---
+++ source.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Spu7Nix/SPWN-language.git
+cd SPWN-language/spwn-lang
+cargo build --release
+mv target/release/spwn ../../
+rm -rf ../../libraries/std
+mv libraries/std ../../libraries/std
+cd ../../
+rm -rf SPWN-language
